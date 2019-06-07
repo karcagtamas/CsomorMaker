4af3c5b10fb152e5b76947063e2f17788d52d5ab
karcagtamas <karcagtamas@gmail.com>
2019-06-07 14:16:08 +0200

    db

--- database/database.sql
+++ database/database.sql
@@ -2,10 +2,30 @@ CREATE DATABASE IF NOT EXISTS csomormaker
 CHARACTER SET utf8
 COLLATE utf8_hungarian_ci;
 
+USE csomormaker;
+
+CREATE TABLE roles(
+  id int(11) NOT NULL,
+  name varchar(100) NOT NULL,
+  accessLevel int(1) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users(
+  id int(11) AUTO_INCREMENT NOT NULL,
+  username varchar(100) NOT NULL,
+  email varchar(255) NOT NULL,
+  name varchar(100) NOT NULL,
+  role int(11) NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_role_roles FOREIGN KEY (role)
+  REFERENCES roles(id)
+);
+
 CREATE TABLE events(
     id int(11) AUTO_INCREMENT NOT NULL,
     name varchar(50) NOT NULL,
-    group int(11) NOT NULL,
+    groupId int(11) NOT NULL,
     isLocked boolean NOT NULL DEFAULT FALSE,
     isDisabled boolean NOT NULL DEFAULT FALSE,
     creater int(11) NOT NULL,
@@ -17,7 +37,7 @@ CREATE TABLE events(
     playerCost decimal NOT NULL DEFAULT 0,
     visitorCost decimal NOT NULL DEFAULT 0,
     playerDeposit decimal NOT NULL DEFAULT 0,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    CONSTRAINT fk_creater_users FOREIGN KEY (creater)
+    REFERENCES users(id) 
 );
-
-CREATE TABLE users();
