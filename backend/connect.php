326e18a3aae750a8adce87b6ab0e9848c704d169
karcagtamas <you@example.com>
2019-06-07 11:48:19 +0200

    php

---
+++ backend/connect.php
@@ -0,0 +1,12 @@
+<?php 
+    define('SERVER' : 'localhost');
+    define('USER' : 'root');
+    define('PASS' : '');
+    define('DATABASE' : 'csomormaker');
+
+    $db = new mysqli(SERVER, USER, PASS, DATABASE);
+
+    if ($db->connect_error){
+        die('Database fail');
+    }
+?>
