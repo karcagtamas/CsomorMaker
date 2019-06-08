6eaee4b0b91ac55c7c5a02100597637e2b983ba7
karcagtamas <karcagtamas@gmail.com>
2019-06-08 17:31:14 +0200

    database frame

--- database/database.sql
+++ database/database.sql
@@ -25,7 +25,6 @@ CREATE TABLE users(
 CREATE TABLE events(
     id int(11) AUTO_INCREMENT NOT NULL,
     name varchar(50) NOT NULL,
-    groupId int(11) NOT NULL,
     isLocked boolean NOT NULL DEFAULT FALSE,
     isDisabled boolean NOT NULL DEFAULT FALSE,
     creater int(11) NOT NULL,
@@ -37,9 +36,25 @@ CREATE TABLE events(
     playerCost decimal NOT NULL DEFAULT 0,
     visitorCost decimal NOT NULL DEFAULT 0,
     playerDeposit decimal NOT NULL DEFAULT 0,
+    days int(2) NOT NULL DEFAULT 1,
+    startHour int(2) NOT NULL DEFAULT 12,
+    endHour int(2) NOT NULL DEFAULT 12,
+    length int(4),
+    ready boolean NOT NULL DEFAULT TRUE,
     PRIMARY KEY(id),
     CONSTRAINT fk_creater_users FOREIGN KEY (creater)
     REFERENCES users(id) 
+);
+
+CREATE TABLE usereventswitch(
+  user int(11) NOT NULL,
+  event int(11) NOT NULL,
+  connectionDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(user, event),
+  CONSTRAINT fk_user_users_usereventswitch FOREIGN KEY (user)
+  REFERENCES users(id),
+  CONSTRAINT fk_event_events_usereventswitch FOREIGN KEY (event)
+  REFERENCES events(id)  
 );
 
 CREATE TABLE payouttypes(
@@ -74,7 +89,7 @@ CREATE TABLE bosses(
 
 CREATE TABLE todoes(
   id int(11) AUTO_INCREMENT NOT NULL,
-  eventId int(11) AUTO_INCREMENT NOT NULL,
+  eventId int(11) NOT NULL,
   date datetime DEFAULT CURRENT_TIMESTAMP,
   text longtext NOT NULL,
   PRIMARY KEY(id),
@@ -93,3 +108,51 @@ CREATE TABLE messages(
   CONSTRAINT fk_event_events_messages FOREIGN KEY (event)
   REFERENCES events (id)
 );
+
+CREATE TABLE works(
+  id int(11) AUTO_INCREMENT NOT NULL,
+  name varchar(50) NOT NULL,
+  event int(11) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_event_events_works FOREIGN KEY (event)
+  REFERENCES events(id)
+);
+
+CREATE TABLE workTables(
+   id int(11) AUTO_INCREMENT NOT NULL,
+   day int(2) NOT NULL,
+   hour int(2) NOT NULL,
+   isActive boolean DEFAULT TRUE,
+   worker int(11),
+   work int(11) NOT NULL,
+   PRIMARY KEY(id),
+   CONSTRAINT fk_work_works_workTables FOREIGN KEY (work)
+   REFERENCES works(id),
+   CONSTRAINT fk_worker_users_workTables FOREIGN KEY (worker)
+   REFERENCES users(id)
+ );
+
+CREATE TABLE workerTables(
+  id int(11) AUTO_INCREMENT NOT NULL,
+  day int(2) NOT NULL,
+  hour int(2) NOT NULL,
+  isAvaiable boolean NOT NULL DEFAULT TRUE,
+  work int(11),
+  worker int(11) NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_work_works_workerTables FOREIGN KEY (work)
+  REFERENCES works(id),
+  CONSTRAINT fk_worker_users_workerTables FOREIGN KEY (worker)
+  REFERENCES users(id)
+);
+
+CREATE TABLE workworkerswitch(
+  worker int(11) NOT NULL,
+  work int(11) NOT NULL,
+  isValid boolean NOT NULL DEFAULT TRUE,
+  PRIMARY KEY(worker, work),
+  CONSTRAINT fk_worker_users_workworkerswitch FOREIGN KEY (worker)
+  REFERENCES users(id),
+  CONSTRAINT fk_work_works_workworkerswitch FOREIGN KEY (work)
+  REFERENCES works(id)
+  );
