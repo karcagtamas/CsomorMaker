7ae816aa01a173903b3dc8a3b4e99497cbaba3eb
karcag.tamas <karcagtamas@gmail.com>
2019-06-14 11:44:07 +0200

    summary - players, visitors

--- frontend/src/app/components/event/event-summary/event-summary/event-summary.component.ts
+++ frontend/src/app/components/event/event-summary/event-summary/event-summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Event } from 'src/app/models';
 
 @Component({
   selector: 'app-event-summary',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-summary.component.scss']
 })
 export class EventSummaryComponent implements OnInit {
+  @Input() event: Event;
+  playerSummary = 0;
+  visitorSumamry = 0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.setPlayerSummary();
+    this.setVisitorSummary();
   }
 
+  setPlayerSummary() {
+    this.playerSummary = this.event.currentPlayers * this.event.playerCost;
+  }
+
+  setVisitorSummary() {
+    this.visitorSumamry = this.event.visitors * this.event.visitorCost;
+  }
 }
