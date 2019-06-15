f253c4c64d09012fe36dbc2a8de92f1b47deee46
karcag.tamas <karcagtamas@gmail.com>
2019-06-15 14:34:23 +0200

    connect

--- backend/connect.php
+++ backend/connect.php
@@ -1,8 +1,16 @@
 <?php 
-    define('SERVER', '127.0.0.1');
-    define('USER', 'karcags');
-    define('PASS', 'SL7INGAsq6HfYKV0');
-    define('DATABASE' , 'karcags_csomormaker');
+
+    if ($_SERVER['SERVER_NAME'] == "localhost"){
+        define('SERVER', '127.0.0.1');
+        define('USER', 'root');
+        define('PASS', '');
+        define('DATABASE' , 'csomormaker');
+    }else{
+        define('SERVER', '127.0.0.1');
+        define('USER', 'karcags');
+        define('PASS', 'SL7INGAsq6HfYKV0');
+        define('DATABASE' , 'karcags_csomormaker');
+    }
 
     $db = new mysqli(SERVER, USER, PASS, DATABASE);
 
