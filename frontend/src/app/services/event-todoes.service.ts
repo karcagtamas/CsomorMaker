7f8a5be657f050abf0cef9d0d8fcb074038aa9b9
karcagtamas <you@example.com>
2019-07-01 14:01:13 +0200

    todoes - half

--- frontend/src/app/services/event-todoes.service.ts
+++ frontend/src/app/services/event-todoes.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Response, EventTodo } from '../models';
+
+const URL = environment.api;
+
+const HttpHeader = { withCredentials: true };
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventTodoesService {
+  url = URL + '/event/todoes';
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getEventTodoes(event: number): Promise<EventTodo[]>{
+    return this.http.get<EventTodo[]>(`${this.url}/get/${event}`, HttpHeader).toPromise();
+  }
+
+  updateEventTodo(todo: number, text: string, importance: number, expiration: Date): Promise<Response>{
+    return this.http.get<Response>(`${this.url}/update/${event}`, HttpHeader).toPromise();
+  }
 }
