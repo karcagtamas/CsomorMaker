555534c6bc571bdb7311f77557dd0134a75b5e3e
karcagtamas <karcagtamas@gmail.com>
2019-07-08 22:24:58 +0200

    new gt

--- frontend/src/app/components/gt/new-gt/new-gt.component.ts
+++ frontend/src/app/components/gt/new-gt/new-gt.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { GtService, NotificationService } from 'src/app/services';
 
 @Component({
   selector: 'app-new-gt',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-gt.component.scss']
 })
 export class NewGtComponent implements OnInit {
+  yearControl = new FormControl('', [Validators.required, Validators.maxLength(50)]);
 
-  constructor() { }
+  constructor(private gtservice: GtService, private notificationservice: NotificationService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addGt() {
+    if (this.yearControl.invalid) {
+      this.notificationservice.warning('Nem megfelelő évszém!');
+    } else {
+      this.gtservice
+        .addGt(+this.yearControl.value)
+        .then(res => {
+          if (res.response === 'success') {
+            this.notificationservice.success(res.message);
+            this.yearControl.setValue('');
+          } else {
+            this.notificationservice.error(res.message);
+          }
+        })
+        .catch(() => {
+          this.notificationservice.error('A gólyatábor létrehozása közben hiba történt! Próbálja késöbb újra!');
+        });
+    }
   }
-
 }
